f9d2ec7254adbb28203e00932cd39123d6b86b7c
wighawag <wighawag@gmail.com>
2023-04-24 23:05:38 +0100

    add --no-parse

--- src/cli.ts
+++ src/cli.ts
@@ -36,7 +36,7 @@ if (!env_mode_name) {
 env_mode_name = env_mode_name || 'MODE';
 
 let mode = process.env[env_mode_name];
-let parse = true; // TODO
+let parse = true;
 let commandArgs: string[] = [];
 let command: string | undefined;
 // basic arg parsing (no long form)
@@ -98,67 +98,69 @@ loadEnv({
 // we set env_mode_name to get the mode
 process.env[env_mode_name] = mode;
 
-const newArgs = commandArgs.map((arg) => {
-	const [prefix, ...list_to_parse] = arg.split('@@');
-	if (list_to_parse.length > 0) {
-		// if there are ny @@ we process them in turn
-		// we also ensure we save the prefix (could be "")
-		// Note that ldenv will not allow you to use @@
-		// TODO allow to escape  @@
-		const combined = list_to_parse
-			.map((to_parse) => {
-				// we get the var_name as first value by splitting via '@:'
-				// the rest is the default value / suffix pair
-				const [var_name, potential_default_value, potential_suffix] = to_parse.split('@:');
+const newArgs = !parse
+	? commandArgs
+	: commandArgs.map((arg) => {
+			const [prefix, ...list_to_parse] = arg.split('@@');
+			if (list_to_parse.length > 0) {
+				// if there are ny @@ we process them in turn
+				// we also ensure we save the prefix (could be "")
+				// Note that ldenv will not allow you to use @@
+				// TODO allow to escape  @@
+				const combined = list_to_parse
+					.map((to_parse) => {
+						// we get the var_name as first value by splitting via '@:'
+						// the rest is the default value / suffix pair
+						const [var_name, potential_default_value, potential_suffix] = to_parse.split('@:');
 
-				if (var_name.length === 0) {
-					error(`error: this is not valid : '@@${to_parse}' please specify an ENV var name after '@@'`);
-				}
+						if (var_name.length === 0) {
+							error(`error: this is not valid : '@@${to_parse}' please specify an ENV var name after '@@'`);
+						}
 
-				const hasSuffix = typeof potential_suffix !== 'undefined';
-				const suffix = hasSuffix ? potential_suffix : potential_default_value;
-				const default_value = hasSuffix ? potential_default_value : undefined;
+						const hasSuffix = typeof potential_suffix !== 'undefined';
+						const suffix = hasSuffix ? potential_suffix : potential_default_value;
+						const default_value = hasSuffix ? potential_default_value : undefined;
 
-				// fallback var_name is allowed, they are separated by ","
-				const var_names = var_name.split(',');
+						// fallback var_name is allowed, they are separated by ","
+						const var_names = var_name.split(',');
 
-				let value;
-				for (const name of var_names) {
-					// each of these var_name can be composed of other env value (no recursion, just one level)
-					const splitted_by_colon = name.split(':');
-					// console.log({ splitted_by_colon })
-					const actual_name = splitted_by_colon
-						.map((v, index) => {
-							if (index % 2 == 0) {
-								return v;
-							} else {
-								return process.env[v];
+						let value;
+						for (const name of var_names) {
+							// each of these var_name can be composed of other env value (no recursion, just one level)
+							const splitted_by_colon = name.split(':');
+							// console.log({ splitted_by_colon })
+							const actual_name = splitted_by_colon
+								.map((v, index) => {
+									if (index % 2 == 0) {
+										return v;
+									} else {
+										return process.env[v];
+									}
+								})
+								.join('');
+							// console.log({ actual_name })
+							value = process.env[actual_name];
+							if (value) {
+								// if we find one of the comma separated list matching, we exit
+								break;
 							}
-						})
-						.join('');
-					// console.log({ actual_name })
-					value = process.env[actual_name];
-					if (value) {
-						// if we find one of the comma separated list matching, we exit
-						break;
-					}
-				}
-				value = value || default_value;
-				if (!value) {
-					error(`
+						}
+						value = value || default_value;
+						if (!value) {
+							error(`
         error: @@${to_parse} was specified in the command but there is no env variable named ${var_name}.
         To prevent this error you can provide a default value with '@@${var_name}@:<default value>@:'
         An empty default can be specified with '@@${var_name}@:@:'
         `);
-				}
-				// console.log({ prefix, var_name, default_value, suffix, value })
-				return value + (suffix || '');
-			})
-			.join('');
-		return prefix + combined;
-	} else {
-		return arg;
-	}
-});
+						}
+						// console.log({ prefix, var_name, default_value, suffix, value })
+						return value + (suffix || '');
+					})
+					.join('');
+				return prefix + combined;
+			} else {
+				return arg;
+			}
+	  });
 
 execFileSync(command!, newArgs, {stdio: ['inherit', 'inherit', 'inherit']});
